da048ad72f421e98eb96b6025bac23b04d096459
Dmitrii Dmitriev <dimple395@gmail.com>
2021-03-10 05:34:47 -0800

    Added EntityFramework

---
+++ GameStore.Domain/Concrete/EFGameRepository.cs
@@ -0,0 +1,16 @@
+﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace GameStore.Domain.Concrete
+{
+    public class EFGameRepository : IGameRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games
+        {
+            get { return context.Games; }
+        }
+    }
+}
--- GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,4 +1,5 @@
 ﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Concrete;
 using GameStore.Domain.Entities;
 using Moq;
 using Ninject;
@@ -28,17 +29,22 @@ namespace GameStore.WebUI.Infrastructure
             return kernel.GetAll(serviceType);
         }
 
+        //private void AddBindings()
+        //{
+        //    // Здесь размещаются привязки
+        //    Mock<IGameRepository> mock = new Mock<IGameRepository>();
+        //    mock.Setup(m => m.Games).Returns(new List<Game>
+        //    {
+        //        new Game { Name = "SimCity", Price = 1499 },
+        //        new Game { Name = "TITANFALL", Price = 2299 },
+        //        new Game { Name = "Battlefield 4", Price = 899.4M }
+        //    });
+        //    kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+        //}
+
         private void AddBindings()
         {
-            // Здесь размещаются привязки
-            Mock<IGameRepository> mock = new Mock<IGameRepository>();
-            mock.Setup(m => m.Games).Returns(new List<Game>
-            {
-                new Game { Name = "SimCity", Price = 1499 },
-                new Game { Name = "TITANFALL", Price = 2299 },
-                new Game { Name = "Battlefield 4", Price = 899.4M }
-            });
-            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
         }
     }
 }
